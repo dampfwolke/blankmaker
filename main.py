31a961eef66463537d37c736b39dbadc7e1869ca
Dampfwolke <isko987@gmail.com>
2025-06-03 11:06:34 +0200

    zeitstempel modul erstellt und vom main script ausgelagert

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
-from datetime import datetime
 
 from PySide6 import QtWidgets as qtw
 from PySide6 import QtCore as qtc
 from PySide6 import QtGui as qtg
+
+from utils.zeitstempel import zeitstempel
 
 from UI.frm_main_window import Ui_frm_main_window
 from UI.animated_tabhelper import AnimatedTabHelper  # <-- Animation importieren
@@ -20,13 +21,9 @@ class MainWindow(qtw.QMainWindow, Ui_frm_main_window):
 
     @qtc.Slot()
     def rechteck_erstellen(self):
-        self.statusBar().showMessage(f"Rechteck wurde erstellt. {self.zeitstempel()}", 5000)
-        print(f"Rechteck wurde erstellt! {self.zeitstempel()}")
+        self.statusBar().showMessage(f"Rechteck wurde erstellt. {zeitstempel(2)}", 5000)
+        print(f"Rechteck wurde erstellt! {zeitstempel(2)}")
 
-    def zeitstempel(self) -> str:
-        aktuelle_zeit = datetime.now()
-        zeitstempel = aktuelle_zeit.strftime("%H:%M:%S")
-        return zeitstempel
 
 if __name__ == "__main__":
     app = qtw.QApplication(sys.argv)
